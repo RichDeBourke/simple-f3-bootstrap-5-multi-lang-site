27c3f7639634f261b622531aff9aaa6ba27e66b2
Rich DeBourke <rdebourke@yahoo.com>
2024-07-23 18:38:03 +0900

    Validate with Bootstrap 5.3.3 and enhance offcanvas navigation.

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
+  optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()],
+  },
   entry: './src/js/main.js',
   output: {
     filename: 'main.min.js',
     path: path.resolve(__dirname, 'js')
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  externals: {
+    bootstrap: {
+      modal: 'Modal',
+      offcanvas: 'Offcanvas'
+    }
+  }
 };
